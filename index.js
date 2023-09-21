131a7ec16c8f79b694cc19e53e2b4ed64af6232b
Muhammad Ismail <112403279+mIsmail-dev@users.noreply.github.com>
2023-09-21 16:35:14 +0500

    updated options

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/export-pdf", async (req, res) => {
 
     const browser = await puppeteer.launch({
       headless: "new",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: ["--no-sandbox"],
     });
 
     const page = await browser.newPage();
